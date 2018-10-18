a18ccf2a74d5597b7a60cbf56459ed975045f900
Manh Tan Nguyen <manh_tan_nguyen@epam.com>
2018-10-18 11:21:07 -0400

    Add some improvements

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,8 @@
-FROM node:7.9.0
+FROM node:alpine
 
-WORKDIR /app
+COPY . /application/
+RUN cd /application/ && \
+    npm install
 
-RUN useradd -ms /bin/bash aws-es-kibana
-RUN chown aws-es-kibana:aws-es-kibana /app
-
-ADD index.js /app
-ADD package.json /app
-
-RUN npm install
-
+ENTRYPOINT ["node", "/application/index.js"]
 EXPOSE 9200
-
-ENTRYPOINT ["node", "index.js"]
--- index.js
+++ index.js
@@ -7,8 +7,10 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var stream = require('stream');
 var figlet = require('figlet');
-var basicAuth = require('basic-auth-connect');
+var basicAuth = require('express-basic-auth');
 var compress = require('compression');
+const fs = require('fs');
+const homedir = require('os').homedir();
 
 var yargs = require('yargs')
     .usage('usage: $0 [options] <aws-es-cluster-endpoint>')
@@ -35,13 +37,13 @@ var yargs = require('yargs')
     })
     .option('u', {
       alias: 'user',
-      default: process.env.USER,
+      default: process.env.AUTH_USER ||process.env.USER,
       demand: false,
       describe: 'the username to access the proxy'
     })
     .option('a', {
       alias: 'password',
-      default: process.env.PASSWORD,
+      default: process.env.AUTH_PASSWORD || process.env.PASSWORD,
       demand: false,
       describe: 'the password to access the proxy'
     })
@@ -50,6 +52,19 @@ var yargs = require('yargs')
       default: false,
       demand: false,
       describe: 'remove figlet banner'
+    })
+    .option('H', {
+        alias: 'health-path',
+        default: process.env.HEALTH_PATH,
+        demand: false,
+        describe: 'URI path for health check',
+        type: 'string'
+    })
+    .option('l', {
+      alias: 'limit',
+      default: process.env.LIMIT || '10000kb',
+      demand: false,
+      describe: 'request limit'
     })
     .help()
     .version()
@@ -66,7 +81,7 @@ if (!ENDPOINT) {
 // Try to infer the region if it is not provided as an argument.
 var REGION = argv.r;
 if (!REGION) {
-    var m = ENDPOINT.match(/\.([^.]+)\.es\.amazonaws\.com\.?$/);
+    var m = ENDPOINT.match(/\.([^.]+)\.es\.amazonaws\.com\.?(?=.*$)/);
     if (m) {
         REGION = m[1];
     } else {
@@ -84,13 +99,22 @@ if (!TARGET.match(/^https?:\/\//)) {
 
 var BIND_ADDRESS = argv.b;
 var PORT = argv.p;
+var REQ_LIMIT = argv.l;
 
 var credentials;
-var chain = new AWS.CredentialProviderChain();
-chain.resolve(function (err, resolved) {
-    if (err) throw err;
-    else credentials = resolved;
-});
+
+var PROFILE = process.env.AWS_PROFILE;
+
+if (!PROFILE) {
+    var chain = new AWS.CredentialProviderChain();
+    chain.resolve(function (err, resolved) {
+        if (err) throw err;
+        else credentials = resolved;
+    });
+} else {
+    credentials = new AWS.SharedIniFileCredentials({profile: PROFILE});
+    AWS.config.credentials = credentials;
+}
 
 function getCredentials(req, res, next) {
     return credentials.get(function (err) {
@@ -98,24 +122,46 @@ function getCredentials(req, res, next) {
         else return next();
     });
 }
-var proxy = httpProxy.createProxyServer({
+
+var options = {
     target: TARGET,
     changeOrigin: true,
     secure: true
-});
+};
+
+var proxy = httpProxy.createProxyServer(options);
 
 var app = express();
 app.use(compress());
-if (argv.u && argv.a) {
-  app.use(basicAuth(argv.u, argv.a));
-}
-app.use(bodyParser.raw({type: function() { return true; }}));
+app.use(bodyParser.raw({limit: REQ_LIMIT, type: function() { return true; }}));
 app.use(getCredentials);
-app.use(function (req, res) {
+
+if (argv.H) {
+    app.get(argv.H, function (req, res) {
+        res.setHeader('Content-Type', 'text/plain');
+        res.send('ok');
+    });
+}
+
+if (argv.u && argv.a) {
+
+  var users = {};
+  var user = process.env.USER || process.env.AUTH_USER;
+  var pass = process.env.PASSWORD || process.env.AUTH_PASSWORD;
+
+  users[user] = pass;
+
+  app.use(basicAuth({
+    users: users,
+    challenge: true
+  }));
+}
+
+app.use(async function (req, res) {
     var bufferStream;
     if (Buffer.isBuffer(req.body)) {
         var bufferStream = new stream.PassThrough();
-        bufferStream.end(req.body);
+        await bufferStream.end(req.body);
     }
     proxy.web(req, res, {buffer: bufferStream});
 });
@@ -129,7 +175,7 @@ proxy.on('proxyReq', function (proxyReq, req) {
     if (Buffer.isBuffer(req.body)) request.body = req.body;
     if (!request.headers) request.headers = {};
     request.headers['presigned-expires'] = false;
-    request.headers['Host'] = ENDPOINT;
+    request.headers['Host'] = endpoint.hostname;
 
     var signer = new AWS.Signers.V4(request, 'es');
     signer.addAuthorization(credentials, new Date());
@@ -158,3 +204,11 @@ if(!argv.s) {
 
 console.log('AWS ES cluster available at http://' + BIND_ADDRESS + ':' + PORT);
 console.log('Kibana available at http://' + BIND_ADDRESS + ':' + PORT + '/_plugin/kibana/');
+if (argv.H) {
+    console.log('Health endpoint enabled at http://' + BIND_ADDRESS + ':' + PORT + argv.H);
+}
+
+fs.watch(`${homedir}/.aws/credentials`, (eventType, filename) => {
+    credentials = new AWS.SharedIniFileCredentials({profile: PROFILE});
+    AWS.config.credentials = credentials;
+});
